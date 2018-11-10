b4f64feed98c74aaa38d4519c5eb2531d5919f9e
MSSQL_Skills <itkrishna1988@gmail.com>
2018-11-11 00:56:46 +0530

    Latest

---
+++ Interview Questions/MSBI Skills/SQL Server Interview Questions & Answers – SET 12(10 Questions & Answers) _ Improving my SQL BI Skills_files/rate(1).php
@@ -0,0 +1,6 @@
+PDRTJS_8181511_comm_1507.nero_up = 0;
+PDRTJS_8181511_comm_1507.nero_dn = 0;		
+PDRTJS_settings_8181511_comm_1507= { "type" : "nero" , "size" : "sml" , "star_color" : "hand" , "custom_star" : "" , "font_size" : "" , "font_line_height" : "16px" , "font_color" : "" , "font_align" : "left" , "font_position" : "right" , "font_family" : "" , "font_bold" : "normal" , "font_italic" : "normal" , "text_vote" : "Vote" , "text_votes" : "Votes" , "text_rate_this" : "Rate This" , "text_1_star" : "Very Poor" , "text_2_star" : "Poor" , "text_3_star" : "Average" , "text_4_star" : "Good" , "text_5_star" : "Excellent" , "text_thank_you" : "Thank You" , "text_rate_up" : "Rate Up" , "text_rate_down" : "Rate Down"  };
+PDRTJS_8181511_comm_1507.init();		
+PDRTJS_8181511_comm_1507.token='a273e6a41e894bc5db129f9598c829e6';
+/*8181511,_comm_1507,wp-comment-1507,3075719923,0-0*/
---
+++ Interview Questions/MSBI Skills/SQL Server Interview Questions & Answers – SET 12(10 Questions & Answers) _ Improving my SQL BI Skills_files/rate(2).php
@@ -0,0 +1,6 @@
+PDRTJS_8181511_comm_1508.nero_up = 0;
+PDRTJS_8181511_comm_1508.nero_dn = 0;		
+PDRTJS_settings_8181511_comm_1508= { "type" : "nero" , "size" : "sml" , "star_color" : "hand" , "custom_star" : "" , "font_size" : "" , "font_line_height" : "16px" , "font_color" : "" , "font_align" : "left" , "font_position" : "right" , "font_family" : "" , "font_bold" : "normal" , "font_italic" : "normal" , "text_vote" : "Vote" , "text_votes" : "Votes" , "text_rate_this" : "Rate This" , "text_1_star" : "Very Poor" , "text_2_star" : "Poor" , "text_3_star" : "Average" , "text_4_star" : "Good" , "text_5_star" : "Excellent" , "text_thank_you" : "Thank You" , "text_rate_up" : "Rate Up" , "text_rate_down" : "Rate Down"  };
+PDRTJS_8181511_comm_1508.init();		
+PDRTJS_8181511_comm_1508.token='a6659f98753e612b043d9c97836de5bc';
+/*8181511,_comm_1508,wp-comment-1508,3075719923,0-0*/
