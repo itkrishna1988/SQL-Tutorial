facbbda50063e0c18541a85046bdf446ef2f6243
MSSQL_Skills <itkrishna1988@gmail.com>
2018-11-11 01:03:10 +0530

    latest

--- Interview Questions/MSBI Skills/SQL Server Interview Questions & Answers – SET 12(10 Questions & Answers) _ Improving my SQL BI Skills_files/rate.php
+++
@@ -1,6 +0,0 @@
-PDRTJS_8181509_post_3473.avg_rating = 5;
-PDRTJS_8181509_post_3473.votes = 1;		
-PDRTJS_settings_8181509_post_3473= { "type" : "stars" , "size" : "sml" , "star_color" : "yellow" , "custom_star" : "" , "font_size" : "" , "font_line_height" : "16px" , "font_color" : "" , "font_align" : "left" , "font_position" : "right" , "font_family" : "" , "font_bold" : "normal" , "font_italic" : "normal" , "text_vote" : "Vote" , "text_votes" : "Votes" , "text_rate_this" : "Rate This" , "text_1_star" : "Very Poor" , "text_2_star" : "Poor" , "text_3_star" : "Average" , "text_4_star" : "Good" , "text_5_star" : "Excellent" , "text_thank_you" : "Thank You" , "text_rate_up" : "Rate Up" , "text_rate_down" : "Rate Down"  };
-PDRTJS_8181509_post_3473.init();		
-PDRTJS_8181509_post_3473.token='974d7a66f00a9525d811b8c9c950819e';
-/*8181509,_post_3473,wp-post-3473,3075719923,5-1*/
